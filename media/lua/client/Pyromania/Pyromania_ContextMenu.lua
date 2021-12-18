f4090ca6e6341832de4cae42e232c7bc07f69a74
Corentin Delorme <codelorme@gmail.com>
2021-12-18 12:30:41 +0100

    Fix spelling/wording: flamable => flammable, ignition => igniter

--- media/lua/client/Pyromania/Pyromania_ContextMenu.lua
+++ media/lua/client/Pyromania/Pyromania_ContextMenu.lua
@@ -3,19 +3,19 @@ function onFillWorldObjectContextMenu(playerId, context, worldobjects, test)
 	local player = getSpecificPlayer(playerId)
 	local inventory = player:getInventory()
 	
-	local ignitions = inventory:getAllEvalRecurse(function(item, player)
+	local igniters = inventory:getAllEvalRecurse(function(item, player)
 		return item:getType() == 'Lighter' or item:getType() == 'Matches'
 	end, ArrayList.new())
 
-	if ignitions:size() <= 0 then
+	if igniters:size() <= 0 then
 		return
 	end
 
-	local flamables = inventory:getAllEvalRecurse(function(item, player)
+	local flammables = inventory:getAllEvalRecurse(function(item, player)
 		return item:getCategory() == 'Literature'
 	end, ArrayList.new())
 
-	if flamables:size() <= 0 then
+	if flammables:size() <= 0 then
 		return
 	end
 
@@ -23,17 +23,15 @@ function onFillWorldObjectContextMenu(playerId, context, worldobjects, test)
 		local square = object:getSquare()
 		if square then
 
-			for i = 0, ignitions:size() - 1 do
+			for i = 0, igniters:size() - 1 do
 
-				local ignition = ignitions:get(i)
-				print("Ignition", ignition:getName())
-				local rootMenu = context:addOption(getText('ContextMenu_SetFire') .. " (" .. ignition:getName() .. ")", worldobjects, nil)
+				local igniter = igniters:get(i)
+				local rootMenu = context:addOption(getText('ContextMenu_SetFire') .. " (" .. igniter:getName() .. ")", worldobjects, nil)
 				local subMenu = context:getNew(context)
 				context:addSubMenu(rootMenu, subMenu)
-				for j = 0, flamables:size() - 1 do
-					local flamable = flamables:get(j)
-					print("Flamable", flamable:getName())
-					subMenu:addOption(flamable:getName(), worldobjects, onSetFire, player, square, ignition, flamable)
+				for j = 0, flammables:size() - 1 do
+					local flammable = flammables:get(j)
+					subMenu:addOption(flammable:getName(), worldobjects, onSetFire, player, square, igniter, flammable)
 				end
 				return
 			end
@@ -41,11 +39,11 @@ function onFillWorldObjectContextMenu(playerId, context, worldobjects, test)
 	end
 end
 
-function onSetFire(worldobjects, player, square, ignition, flamable)
+function onSetFire(worldobjects, player, square, igniter, flammable)
 	if luautils.walkAdj(player, square, false) then
-		ISWorldObjectContextMenu.equip(player, player:getPrimaryHandItem(), ignition, true, false)
-		ISWorldObjectContextMenu.equip(player, player:getSecondaryHandItem(), flamable, false, false)
-		ISTimedActionQueue.add(SetFireAction:new(player, square, ignition, flamable, 100))
+		ISWorldObjectContextMenu.equip(player, player:getPrimaryHandItem(), igniter, true, false)
+		ISWorldObjectContextMenu.equip(player, player:getSecondaryHandItem(), flammable, false, false)
+		ISTimedActionQueue.add(SetFireAction:new(player, square, igniter, flammable, 100))
 	end
 end
 
--- media/lua/client/Pyromania/Pyromania_SetFireAction.lua
+++ media/lua/client/Pyromania/Pyromania_SetFireAction.lua
@@ -25,21 +25,21 @@ end
 function SetFireAction:perform()
 	ISBaseTimedAction.perform(self)
 	self.character:setSecondaryHandItem(nil)
-	self.character:getInventory():Remove(self.flamable)
+	self.character:getInventory():Remove(self.flammable)
 	local fire = IsoFire.new(getCell(), self.square)
 	fire:AttachAnim("Fire", "01", 4, IsoFireManager.FireAnimDelay, -16, -78, true, 0, false, 0.7, IsoFireManager.FireTintMod)
 	self.square:AddTileObject(fire)
 	--self.square:transmitAddObjectToSquare(fire)
 end
 
-function SetFireAction:new(character, square, ignition, flamable, time)
+function SetFireAction:new(character, square, igniter, flammable, time)
 	local o = {}
 	setmetatable(o, self)
 	self.__index = self
 	o.character = character
 	o.square = square
-	o.ignition = ignition
-	o.flamable = flamable
+	o.igniter = igniter
+	o.flammable = flammable
 	o.maxTime = time
 	if o.character:isTimedActionInstant() then o.maxTime = 1 end
 	return o
