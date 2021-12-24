2195738a5a625a3ac71cbf0fb7a545a16ef04e3b
Corentin Delorme <codelorme@gmail.com>
2021-12-24 10:24:21 +0100

    Aggregate igniters and flammables

--- media/lua/client/Pyromania/Pyromania_ContextMenu.lua
+++ media/lua/client/Pyromania/Pyromania_ContextMenu.lua
@@ -1,24 +1,70 @@
 
-PyromaniaWorldObjectContextMenu = {}
+Pyromania = {}
 
-PyromaniaWorldObjectContextMenu.onFillWorldObjectContextMenu = function(playerId, context, worldobjects, test)
+Pyromania.getIgniters = function(inventory)
 
-	local player = getSpecificPlayer(playerId)
-	local inventory = player:getInventory()
-	
-	local igniters = inventory:getAllEvalRecurse(function(item, player)
+	local igniters = {}
+
+	local igniterItems = inventory:getAllEvalRecurse(function(item, player)
 		return item:getType() == 'Lighter' or item:getType() == 'Matches'
 	end, ArrayList.new())
 
-	if igniters:size() <= 0 then
-		return
+	if igniterItems:size() == 0 then
+		return nil
 	end
 
-	local flammables = inventory:getAllEvalRecurse(function(item, player)
+	for i = 0, igniterItems:size() - 1 do
+
+		local igniter = igniterItems:get(i)
+		local name = igniter:getName()
+		if not igniters[name] then
+			igniters[name] = {}
+		end
+
+		table.insert(igniters[name], igniter)
+	end
+
+	return igniters
+end
+
+Pyromania.getFlammables = function(inventory)
+
+	local flammables = {}
+
+	local flammableItems = inventory:getAllEvalRecurse(function(item, player)
 		return item:getCategory() == 'Literature'
 	end, ArrayList.new())
 
-	if flammables:size() <= 0 then
+	if flammableItems:size() == 0 then
+		return nil
+	end
+
+	for i = 0, flammableItems:size() - 1 do
+
+		local flammable = flammableItems:get(i)
+		local name = flammable:getName()
+		if not flammables[name] then
+			flammables[name] = {}
+		end
+
+		table.insert(flammables[name], flammable)
+	end
+
+	return flammables
+end
+
+Pyromania.onFillWorldObjectContextMenu = function(playerId, context, worldobjects, test)
+
+	local player = getSpecificPlayer(playerId)
+	local inventory = player:getInventory()
+
+	local igniters = Pyromania.getIgniters(inventory)
+	if not igniters then
+		return
+	end
+
+	local flammables = Pyromania.getFlammables(inventory)
+	if not flammables then
 		return
 	end
 
@@ -26,23 +72,21 @@ PyromaniaWorldObjectContextMenu.onFillWorldObjectContextMenu = function(playerId
 		local square = object:getSquare()
 		if square then
 
-			for i = 0, igniters:size() - 1 do
+			for igniterName, igniterValues in  pairs(igniters) do
 
-				local igniter = igniters:get(i)
-				local rootMenu = context:addOption(getText('ContextMenu_SetFire') .. " (" .. igniter:getName() .. ")", worldobjects, nil)
+				local rootMenu = context:addOption(getText('ContextMenu_SetFire') .. " [" .. igniterName .. " (" .. tostring(#igniterValues) .. ")]", worldobjects, nil)
 				local subMenu = context:getNew(context)
 				context:addSubMenu(rootMenu, subMenu)
-				for j = 0, flammables:size() - 1 do
-					local flammable = flammables:get(j)
-					subMenu:addOption(flammable:getName(), worldobjects, PyromaniaWorldObjectContextMenu.onSetFire, player, square, igniter, flammable)
+				for flammableName, flammableValues in pairs(flammables) do
+					subMenu:addOption(flammableName .. " (" .. tostring(#flammableValues) .. ")", worldobjects, Pyromania.onSetFire, player, square, igniterValues[1], flammableValues[1])
 				end
-				return
 			end
+			return
 		end
 	end
 end
 
-PyromaniaWorldObjectContextMenu.onSetFire = function(worldobjects, player, square, igniter, flammable)
+Pyromania.onSetFire = function(worldobjects, player, square, igniter, flammable)
 	if luautils.walkAdj(player, square, false) then
 		ISWorldObjectContextMenu.equip(player, player:getPrimaryHandItem(), igniter, true, false)
 		ISWorldObjectContextMenu.equip(player, player:getSecondaryHandItem(), flammable, false, false)
@@ -50,4 +94,4 @@ PyromaniaWorldObjectContextMenu.onSetFire = function(worldobjects, player, squar
 	end
 end
 
-Events.OnFillWorldObjectContextMenu.Add(PyromaniaWorldObjectContextMenu.onFillWorldObjectContextMenu)
+Events.OnFillWorldObjectContextMenu.Add(Pyromania.onFillWorldObjectContextMenu)
